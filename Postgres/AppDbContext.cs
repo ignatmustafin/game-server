4f4c141bbbe5e3fe5a926ca05217256090f460f9
Ignat Mustafin <ignatmustafin77@gmail.com>
2023-05-24 17:06:17 +0300

    #0 edited db host for docker build

--- Postgres/AppDbContext.cs
+++ Postgres/AppDbContext.cs
@@ -13,6 +13,6 @@ public class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=game;Username=macbookair;Password=admin");
+        optionsBuilder.UseNpgsql("Host=postgres;Port=5432;Database=game;Username=macbookair;Password=admin");
     }
 }
