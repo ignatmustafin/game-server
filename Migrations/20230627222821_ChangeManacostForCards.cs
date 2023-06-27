9762eeac26c49d38088c8d3c74ca15dd36c59a0b
Ignat Mustafin <ignatmustafin77@gmail.com>
2023-06-28 02:39:24 +0300

    #0 edited card params

---
+++ Migrations/20230627222821_ChangeManacostForCards.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeManacostForCards : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE ""Card"" SET ""Manacost"" = 3 WHERE ""Id"" IN (3, 27);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE ""Card"" SET ""Manacost"" = 2 WHERE ""Id"" IN (3, 27);");
+        }
+    }
+}
